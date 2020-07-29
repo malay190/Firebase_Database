a36593efcc5b342c85e29b555d359421895c005b
malay190 <malaykumar089@gmail.com>
2020-07-29 19:50:00 +0530

    Retrieve image from Firebase and OnclickListenser is added to the displayed images.

--- app/src/main/java/com/example/firebase_database/ImageActivity.java
+++ app/src/main/java/com/example/firebase_database/ImageActivity.java
@@ -2,15 +2,67 @@ package com.example.firebase_database;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ImageActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private ImageAdapter mAdapter;
+    private ProgressBar mProgressCircle;
+    private DatabaseReference mDatabaseRef;
+    private List<Upload> mUploads;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image);
+
+        mRecyclerView = findViewById(R.id.recycle_view);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mProgressCircle = findViewById(R.id.progress_circle);
+        mUploads = new ArrayList<>();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference("uploads");
+
+        mDatabaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    Upload upload = postSnapshot.getValue(Upload.class);
+                    mUploads.add(upload);
+
+                    mAdapter = new ImageAdapter(ImageActivity.this, mUploads);
+                    mRecyclerView.setAdapter(mAdapter);
+                    mProgressCircle.setVisibility(View.INVISIBLE);
+                }
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(ImageActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                mProgressCircle.setVisibility(View.INVISIBLE);
+
+            }
+        });
     }
+
+
 }
--- app/src/main/java/com/example/firebase_database/ImageAdapter.java
+++ app/src/main/java/com/example/firebase_database/ImageAdapter.java
@@ -1,7 +1,10 @@
 package com.example.firebase_database;
 
 import android.content.Context;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -17,6 +20,12 @@ import java.util.List;
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageHolder> {
     private Context mContext;
     private List<Upload> mUpload;
+    private OnItemClickListener mListener;
+
+    public ImageAdapter(Context context, List<Upload> uploads) {
+        mContext = context;
+        mUpload = uploads;
+    }
 
     @NonNull
     @Override
@@ -41,7 +50,7 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageHolder>
         return mUpload.size();
     }
 
-    public class ImageHolder extends RecyclerView.ViewHolder{
+    public class ImageHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener, View.OnClickListener, MenuItem.OnMenuItemClickListener {
         private TextView textViewName;
         private ImageView imageView;
 
@@ -51,6 +60,63 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageHolder>
             textViewName = itemView.findViewById(R.id.text_view_name);
             imageView = itemView.findViewById(R.id.image_view);
 
+            imageView.setOnClickListener(this);
+            imageView.setOnCreateContextMenuListener(this);
+
         }
+
+        @Override
+        public void onClick(View v) {
+            if (mListener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+                    mListener.onItemClick(position);
+                }
+            }
+        }
+
+        @Override
+        public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+            menu.setHeaderTitle("Menu");
+            MenuItem doWhatever = menu.add(Menu.NONE, 1, 1, "Do whatever");
+            MenuItem delete = menu.add(Menu.NONE, 2, 2, "Delete");
+
+            doWhatever.setOnMenuItemClickListener(this);
+            delete.setOnMenuItemClickListener(this);
+
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            if (mListener != null) {
+                int position = getAdapterPosition();
+                if (position != RecyclerView.NO_POSITION) {
+
+                    switch (item.getItemId()) {
+                        case 1:
+                            mListener.onDoWhatEverClick(position);
+                            return true;
+                        case 2:
+                            mListener.onDeleteClick(position);
+                            return true;
+                    }
+
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+
+        void onDoWhatEverClick(int position);
+
+        void onDeleteClick(int position);
+    }
+
+    public void setOnClickListener(OnItemClickListener listener) {
+        mListener = listener;
     }
 }
